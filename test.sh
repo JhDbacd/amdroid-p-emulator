ba695fef5105b493a2569b9ebf6f1e23632a922d
JhDbacd <lorraime89@gmail.com>
2024-11-18 03:48:33 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.google.android.art
-/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.android.vending 
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.google.android.art | grep Version
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.android.vending | grep Version
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell getprop | grep security_patch
