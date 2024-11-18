c8a205f8a3764b20ff1fdb86ec61fa8d7e99c74a
JhDbacd <lorraime89@gmail.com>
2024-11-18 03:33:18 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
-avdmanager list device
-/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.google.android.art
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.android.vending 
