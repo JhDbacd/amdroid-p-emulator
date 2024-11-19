51c4c3d88f1f685fcf8bb1a4da43786afde566b2
JhDbacd <lorraime89@gmail.com>
2024-11-19 17:06:20 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 sudo apt install binutils -y
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /system/lib64/libandroid_runtime.so .
 readelf -s libandroid_runtime.so | grep JavaVM
-usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.google.android.art | grep Version
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell dumpsys package com.google.android.art | grep Version
