e14529638c23c6c884211a79b6006d10eae0746b
JhDbacd <lorraime89@gmail.com>
2024-11-19 17:38:53 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ sudo apt install binutils -y
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /system/lib64/libandroid_runtime.so .
 readelf -s libandroid_runtime.so | grep JavaVM
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /apex/com.android.art/lib64/libart.so .
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /apex/com.android.runtime/lib64/libart.so .
 readelf -s libart.so | grep _ZN3art6Thread15RunFlipFunctionEPS0_b
