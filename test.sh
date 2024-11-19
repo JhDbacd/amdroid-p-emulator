eff3aaa8802ac02838285b1c4e4e1a500db0e5ae
JhDbacd <lorraime89@gmail.com>
2024-11-19 16:07:38 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell  getprop ro.build.version.sdk
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/lib/
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/lib64/
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/lib/ | grep libandroid_runtime.so 
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/lib64/ | grep libandroid_runtime.so 
