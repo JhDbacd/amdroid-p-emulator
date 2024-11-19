e78c27a5f182904fcf9e4efca2f090d8c8aa60a3
JhDbacd <lorraime89@gmail.com>
2024-11-19 16:28:42 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,3 +5,6 @@
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/lib64/
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/lib/ | grep libandroid_runtime.so 
 /usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/lib64/ | grep libandroid_runtime.so 
+sudo apt install binutils -y
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /system/lib64/libandroid_runtime.so .
+readelf -s libandroid_runtime.so | grep JavaVM
