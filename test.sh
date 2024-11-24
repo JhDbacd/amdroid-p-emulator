247d93fdb9b26408fba96f756c30a89c3870639c
JhDbacd <lorraime89@gmail.com>
2024-11-24 11:14:16 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@
 #readelf -s libandroid_runtime.so | grep JavaVM
 #/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /apex/com.android.art/lib64/libart.so .
 #readelf -s -W -C libart.so | grep RunFlipFun
-/usr/local/lib/android/sdk/platform-tools/adb emulator-5554 shell ls /system/bin/
+/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell ls /system/bin/
