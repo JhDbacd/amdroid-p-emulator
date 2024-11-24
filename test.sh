8a0dbd8e960b157d1ba866dfc9a946a3a4aa5fce
JhDbacd <lorraime89@gmail.com>
2024-11-24 10:55:27 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell  getprop ro.build.version.sdk
-sudo apt install binutils -y
-/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /system/lib64/libandroid_runtime.so .
-readelf -s libandroid_runtime.so | grep JavaVM
-/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /apex/com.android.art/lib64/libart.so .
-readelf -s -W -C libart.so | grep RunFlipFun
+#/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 shell  getprop ro.build.version.sdk
+#sudo apt install binutils -y
+#/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /system/lib64/libandroid_runtime.so .
+#readelf -s libandroid_runtime.so | grep JavaVM
+#/usr/local/lib/android/sdk/platform-tools/adb -s emulator-5554 pull /apex/com.android.art/lib64/libart.so .
+#readelf -s -W -C libart.so | grep RunFlipFun
+/usr/local/lib/android/sdk/platform-tools/adb emulator-5554 shell screencap /sdcard/screenshot.png
+/usr/local/lib/android/sdk/platform-tools/adb pull /sdcard/screenshot.png .
